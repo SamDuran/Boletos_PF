33c621272d7393600bf46babdee0ec4e05354123
SamDuran <94940561+SamDuran@users.noreply.github.com>
2023-06-23 14:14:11 -0400

    Create Compras.cs

---
+++ BoletosAPI/Models/Compras.cs
@@ -0,0 +1,20 @@
+﻿namespace BoletosAPI.Models
+{
+    public class Compras
+    {
+        public int CompraId { get; set; }
+        public double Total { get; set; }
+        public DateTime FechaCompra { get; set; } = DateTime.Now;
+        public string UserEmail { get; set; } = string.Empty;
+        public List<dCompras> dCompras { get; set; } = new List<dCompras>();
+    }
+
+    public class dCompras
+    {
+        public int Id { get; set; }
+        public double Precio { get; set; }
+        public int Cantidad { get; set; }
+        public int CompraId { get; set; }
+        public int BoletoId { get; set; }
+    }
+}
