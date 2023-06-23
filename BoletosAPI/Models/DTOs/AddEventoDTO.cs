fd442126656a4c69fc69cb29a0af7b1328171d3c
SamDuran <94940561+SamDuran@users.noreply.github.com>
2023-06-23 14:14:08 -0400

    Create AddEventoDTO.cs

---
+++ BoletosAPI/Models/DTOs/AddEventoDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace BoletosAPI.Models.DTOs
+{
+    public class AddEventoDTO
+    {
+        public string NombreEvento { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public int UserId { get; set; }
+        public int CategoriaId { get; set; }
+        public int ubicacionId { get; set; }
+        public int Boletos { get; set; }
+        public DateTime fechaEvento { get; set; }
+        public List<AddSeccionDTO> Secciones { get; set; } = new List<AddSeccionDTO>();
+    }
+}
