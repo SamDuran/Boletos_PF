70869d8b96d89b6149bf773606e47932736b0c08
SamDuran <94940561+SamDuran@users.noreply.github.com>
2023-06-23 14:13:57 -0400

    Create NewUserDTO.cs

---
+++ BoletosAPI/Models/DTOs/NewUserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BoletosAPI.Models.DTOs
+{
+    public class NewUserDTO
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public string UserClave { get; set; } = string.Empty;
+    }
+}
