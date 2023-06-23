ea74396260917531369e27a157a704f07d77a79a
SamDuran <94940561+SamDuran@users.noreply.github.com>
2023-06-23 14:13:50 -0400

    Create Ubicaciones.cs

---
+++ BoletosAPI/Models/Ubicaciones.cs
@@ -0,0 +1,11 @@
+﻿namespace BoletosAPI.Models
+{
+    public class Ubicaciones
+    {
+        public int UbicacionId { get; set; }
+        public string Ubicacion { get; set; } = String.Empty;
+        public double? Latitud { get; set; }
+        public double? Longitud { get; set;}
+        public string? Especificaciones { get; set; }
+    }
+}
