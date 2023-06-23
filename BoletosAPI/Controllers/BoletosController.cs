ec1e1e36c33cecf33bd87ab48c18243972c1254f
SamDuran <94940561+SamDuran@users.noreply.github.com>
2023-06-23 14:13:33 -0400

    Update BoletosController.cs

--- BoletosAPI/Controllers/BoletosController.cs
+++ BoletosAPI/Controllers/BoletosController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using BoletosAPI.Models;
 using MySql.Data.MySqlClient;
 using Dapper;
@@ -10,6 +9,9 @@ namespace BoletosAPI.Controllers
     [ApiController]
     public class BoletosController : ControllerBase
     {
+
+        #region PROPS & CONSTRUCTOR
+
         private readonly IConfiguration config;
 
         public BoletosController(IConfiguration _config)
@@ -17,15 +19,40 @@ namespace BoletosAPI.Controllers
             this.config = _config;
         }
 
+        #endregion
+
+        #region GETs
+
+        /* GET ALL MY TICKETS FROM A EVENT */
+        /// <summary>
+        /// This method returns the tickets of the user matching the given UserId and of a specific event matching the given EventoId.
+        /// </summary>
+        /// <param name="userId">UserId's user who bought the tickets</param>
+        /// <param name="eventoId">Event's eventId that was for the ticket</param>
+        /// <returns>Returns true if the creation was success and false if not.</returns>
         [HttpGet]
-        public async Task<ActionResult<List<Boletos>>> GetBoletos()
+        public async Task<ActionResult<List<Boletos>>> GetBoletos(int userId, int eventoId)
         {
             using var connection = new MySqlConnection(config.GetConnectionString("RemoteConnection"));
 
-            var boletos = await connection.QueryAsync<Boletos>("Select * from Boletos");
+            var boletos = await connection.QueryAsync<Boletos>("SELECT * FROM Boletos");
 
             return Ok(boletos);
-
         }
+
+
+        [HttpGet("id")]
+        public async Task<ActionResult<Boletos>> GetBoleto(int id)
+        {
+            using var connection = new MySqlConnection(config.GetConnectionString("RemoteConnection"));
+
+            var boleto = await connection.QueryFirstAsync<Boletos>($"SELECT * FROM Boletos WHERE BoletoId = {id}");
+
+            return Ok(boleto);
+        }
+        #endregion
+
+
+
     }
 }
