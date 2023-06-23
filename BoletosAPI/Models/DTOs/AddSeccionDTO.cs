e03f2a6f71fa6a3b3762ab1254f77f3304cdd129
SamDuran <94940561+SamDuran@users.noreply.github.com>
2023-06-23 14:14:05 -0400

    Create AddSeccionDTO.cs

---
+++ BoletosAPI/Models/DTOs/AddSeccionDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace BoletosAPI.Models.DTOs
+{
+    public class AddSeccionDTO
+    {
+        public string SeccionNombre { get; set; } = string.Empty;
+        public double Precio { get; set; }
+    }
+}
