98251370f55b1404c733ef79f66dbfe493b5ebbe
SamDuran <94940561+SamDuran@users.noreply.github.com>
2023-06-23 14:13:47 -0400

    Create Usuarios.cs

---
+++ BoletosAPI/Models/Usuarios.cs
@@ -0,0 +1,10 @@
+﻿namespace BoletosAPI.Models
+{
+    public class Usuarios
+    {
+        public int UserId { get; set; }
+        public string UserNombre { get; set; } = String.Empty;
+        public string UserEmail { get; set; } = String.Empty;
+        public string UserClave { get; set; } = String.Empty;
+    }
+}
