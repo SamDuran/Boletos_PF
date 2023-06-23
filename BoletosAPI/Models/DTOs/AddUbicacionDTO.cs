71305e98bbd5a0f212e6c1eaaf3cadfa15506c2b
SamDuran <94940561+SamDuran@users.noreply.github.com>
2023-06-23 14:14:01 -0400

    Create AddUbicacionDTO.cs

---
+++ BoletosAPI/Models/DTOs/AddUbicacionDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BoletosAPI.Models.DTOs
+{
+    public class AddUbicacionDTO
+    {
+        public string Ubicacion { get; set; } = string.Empty;
+        public double Latitud { get; set; }
+        public double Longitud { get; set; }
+        public string Especificaciones { get; set; }
+    }
+}
