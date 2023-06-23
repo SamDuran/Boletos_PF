2916444b814f1b4bc3f7661ef2460c77e41c9206
SamDuran <94940561+SamDuran@users.noreply.github.com>
2023-06-23 14:13:52 -0400

    Create Secciones.cs

---
+++ BoletosAPI/Models/Secciones.cs
@@ -0,0 +1,10 @@
+﻿namespace BoletosAPI.Models
+{
+    public class Secciones
+    {
+        public int SeccionId { get; set; }
+        public int EventoId { get; set; }
+        public string Seccion { get; set; } = String.Empty;
+        public double Precio { get; set; }
+    }
+}
