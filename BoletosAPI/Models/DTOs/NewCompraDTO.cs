15e86a8799d0a169ba7574b10a195bb999ce6343
SamDuran <94940561+SamDuran@users.noreply.github.com>
2023-06-23 14:14:00 -0400

    Create NewCompraDTO.cs

---
+++ BoletosAPI/Models/DTOs/NewCompraDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace BoletosAPI.Models.DTOs
+{
+    public class NewCompraDTO
+    {
+        public string UserEmail { get; set; } = string.Empty;
+        public List<NewCompraDTODetails> Details { get; set; } = new List<NewCompraDTODetails>();
+    }
+    public class  NewCompraDTODetails
+    {
+        public double Precio { get; set; }
+        public int Cantidad { get; set; }
+        public int BoletoId { get; set; }
+    }
+}
