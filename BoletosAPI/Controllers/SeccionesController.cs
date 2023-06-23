acf90607c3a7ad2c40d4640d186cf5018613cf75
SamDuran <94940561+SamDuran@users.noreply.github.com>
2023-06-23 14:14:23 -0400

    Create SeccionesController.cs

---
+++ BoletosAPI/Controllers/SeccionesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using BoletosAPI.Models.DTOs;
+using BoletosAPI.Utilities;
+using BoletosAPI.Models;
+using Dapper;
+
+namespace BoletosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeccionesController : ControllerBase
+    {
+        #region Props & Constructor
+        private readonly string conStr;
+
+        public SeccionesController(IConfiguration _config)
+        {
+            conStr = _config.GetConnectionString("RemoteConnection");
+        }
+        #endregion
+
+        
+        [HttpGet("byEventoId/{id}")]
+        public async Task<ActionResult<List<Secciones>>> GetSecciones(int id)
+        {
+            using var connection = new MySqlConnection(conStr);
+
+            var sql = $"SELECT * FROM dSecciones WHERE EventoId = {id} ORDER BY SeccionId";
+
+            var secciones = await connection.QueryAsync<List<Secciones>>(sql);
+
+            return Ok(secciones);
+        }
+
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Secciones>> GetSeccion(int id)
+        {
+            using var connection = new MySqlConnection(conStr);
+
+            var sql = $"SELECT * FROM dSecciones WHERE SeccionId = {id} LIMIT 1;";
+
+            var secciones = await connection.QueryAsync<Secciones?>(sql);
+
+            if(secciones.Count() == 0)
+                return NotFound();
+
+            return Ok(secciones);
+        }
+
+
+       
+       
+    }
+}
